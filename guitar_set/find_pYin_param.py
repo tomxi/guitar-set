089f97a15b4dea60aec35c7693df118e5fa1ad01
Qingyang (Tom) Xi <tomxi007@gmail.com>
2017-11-13 00:34:31 -0500

    created script to guide pYin parameter search

--- guitar_set/find_pYin_param.py
+++ guitar_set/find_pYin_param.py
@@ -7,46 +7,51 @@ import string
 base_dir = '/Users/tom/Music/DataSet/test-set_cleaned/'
 
 
-N = 4
-again = True
-while again:
-    random_dir = ''.join(
-        random.choice(string.ascii_uppercase + string.digits) for _ in
-        range(N))
-    out_base_dir = os.path.join(base_dir, random_dir)
-    try:
-        os.mkdir(out_base_dir)
-        again = False
-    except OSError:
-        continue
-
-
 def random_param():
-    # random param
-    return None
+    param = {"threshdistr": 2,
+             "lowampsuppression": random.uniform(0,0.2),
+             "outputunvoiced": 0,
+             "precisetime": 0,
+             "prunethresh": random.uniform(0,1),
+             "onsetsensitivity": random.uniform(0,1)}
+
+    return param
 
 
-#update pYin_param
-with open('resources/pYin_param.json', 'rw') as p:
-    param = random_param()
-    json.dump(param, p)
+while True:
+    N = 4
+    again = True
+    while again:
+        random_dir = ''.join(
+            [random.choice(string.ascii_uppercase + string.digits) for _ in
+            range(N)]
+        )
+        out_base_dir = os.path.join(base_dir, random_dir)
+        try:
+            os.mkdir(out_base_dir)
+            again = False
+        except OSError:
+            continue
 
+    # update pYin_param
+    with open('resources/pYin_param.json', 'w') as p:
+        param = random_param()
+        json.dump(param, p)
 
-todo_dir_list = [f for f in os.listdir(base_dir) if f.endswith(".wav")]
+    todo_dir_list = [f for f in os.listdir(base_dir) if f.endswith(".wav")]
 
-print(todo_dir_list)
+    print(param, todo_dir_list)
 
-todo_num = len(todo_dir_list)
-for todo_hex in todo_dir_list:
-    print([todo_hex, todo_num, param])
-    out_path = os.path.join(out_base_dir, todo_hex.split('.')[0] + '.jams')
-    hex_path = os.path.join(base_dir, todo_hex)
-    jam = ann.transcribe_hex(hex_path)
-    print('saving jams to {}'.format(out_path))
-    jam.save(out_path)
-    todo_num -= 1
+    todo_num = len(todo_dir_list)
+    for todo_hex in todo_dir_list:
+        print(todo_hex, random_dir, todo_num)
+        out_path = os.path.join(out_base_dir, todo_hex.split('.')[0] + '.jams')
+        hex_path = os.path.join(base_dir, todo_hex)
+        jam = ann.transcribe_hex(hex_path)
+        print('saving jams to {}'.format(out_path))
+        jam.save(out_path)
+        todo_num -= 1
 
-
-with open(out_base_dir+'param.json', 'w') as stream:
-    json.dump(param, stream)
+    with open(os.path.join(out_base_dir,'param.json'), 'w') as stream:
+        json.dump(param, stream)
 
